62d3bf4a22b20ecf47cfe4ad27b01551bbec851c
kasperpirc <kasper@pirc.cc>
2025-03-19 17:34:12 +0100

    Initial commit

---
+++ prisma/migrations/20250310222050_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "Animal" (
+    "animal_id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "image_url" TEXT
+);
